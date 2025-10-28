f7c29ebff5ea59aa010e06e4b1c574ce79799bf6
user.name "Domasiedu <user.email "domasiedu18@gmail.com>
2025-10-28 18:42:10 -0230

    chat model setup

---
+++ database/models/chat-model.ts
@@ -0,0 +1,13 @@
+import {Schema, model, models} from 'mongoose';
+
+interface IChat {
+    title: string;
+    escalated: boolean
+}
+
+const chatSchema = new Schema<IChat>({
+    title: { type: String, required: true },
+    escalated: { type: Boolean, default: false },
+}, { timestamps: true });
+
+export const ChatModel = models.User<IChat> ||    model<IChat>('Chat', chatSchema);
