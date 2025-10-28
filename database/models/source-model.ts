8209438f5b67551b040049d352ea14f5e394f9bc
user.name "Domasiedu <user.email "domasiedu18@gmail.com>
2025-10-28 18:43:42 -0230

    sources schema

---
+++ database/models/source-model.ts
@@ -0,0 +1,15 @@
+import {Schema, model, models} from 'mongoose';
+
+interface ISource {
+    sourceId: string
+    sourceName?: string,
+    sourceType?: string,
+}
+
+const sourceSchema = new Schema<ISource>({
+    sourceId: { type: String, required: true },
+    sourceName: String,
+    sourceType: String,
+}, { timestamps: true });
+
+export const SourceModel = models.User<ISource> ||  model<ISource>('Source', sourceSchema)
